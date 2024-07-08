e362432d05dc7e5c94e03820a77d402355875679
Gaurav Joshi <79218309+gauravmbjoshi@users.noreply.github.com>
2024-07-08 23:32:05 +0530

    create header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import Projects from "./pages/Projects";
 import Dashboard from "./pages/Dashboard";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route
           exact
