cee9bd1612f5997d47c09fe983b06c2067bd4270
Gaurav Joshi <79218309+gauravmbjoshi@users.noreply.github.com>
2024-07-28 16:11:25 +0530

    simple ui changes in DashPosts file

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -72,7 +72,7 @@ export default function DashPosts() {
   };
 
   return (
-    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+    <div className='table-auto w-full overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
         <div className='overflow-x-auto'>
           <Table
