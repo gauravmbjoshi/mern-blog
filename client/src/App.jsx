9985f865e27f09b8a343b7a740fd4276d75909b0
Gaurav Joshi <79218309+gauravmbjoshi@users.noreply.github.com>
2024-07-29 22:39:27 +0530

    add comment section to the post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,9 +14,11 @@ import PrivateRouteAdmin from "./components/PrivateRouteAdmin";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from './pages/UpdatePost';
 import PostPage from './pages/PostPage';
+import ScrollToTop from './components/ScrollToTop';
 function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <Header />
       <Routes>
         <Route
---
+++ client/src/components/CommentSection.jsx
@@ -0,0 +1,90 @@
+import { Button, Textarea, Alert } from 'flowbite-react';
+import { useState } from 'react';
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+export default function CommentSection({postId}) {
+const {currentUser} = useSelector(state => state.user)
+const [comment, setComment] = useState('');
+const [error,setError]=useState(null);
+const handleAddComment = async(e)=>{
+    e.preventDefault();
+    if(comment.length>200){
+        return
+    }
+    try{
+        const res = await fetch(`/api/comment/create`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                content: comment,
+                postId:postId,
+                userId: currentUser._id
+            }),
+        });
+            const data = await res.json();
+            if(res.ok){
+                setComment('');
+                setError(null);
+            }
+    }catch(err){
+        setError(err.message);
+    }
+}
+  return (
+    <div className='max-w-3xl mx-auto w-full my-5 p-3 border rounded border-slate-300'>
+      {currentUser ? (
+        <div className='flex gap-1 items-center m-2 text-sm'>
+          <p>Signed in as:</p>
+          <img
+            className='rounded-full w-5 h-5 object-cover'
+            src={currentUser.profilePicture}
+          />
+          <Link
+            to={"/dashboard?tab=profile"}
+            className='text-xs text-cyan-600 hover:text-cyan-500'
+          >
+            @{currentUser.username}
+          </Link>
+        </div>
+      ) : (
+        <div className='flex gap-1 m-2 items-center text-sm text-cyan-600'>
+          <p>Please sign in to comment.</p>
+          <Link
+            to='/sign-in'
+            className='text-blue-400 hover:text-blue-300'
+          >
+            Sign In
+          </Link>
+        </div>
+      )}
+      {currentUser && (
+        <form onSubmit={handleAddComment}>
+          <Textarea
+            placeholder='Add a comment...'
+            rows='3'
+            maxLength='200'
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
+          />
+          <div className='flex justify-between items-center my-3'>
+            <p className='text-xs text-gray-500'>
+              {200 - comment.length} Character remaining.
+            </p>
+            <Button
+              outline
+              gradientDuoTone='purpleToBlue'
+              type='submit'
+            >
+              Submit
+            </Button>
+          </div>
+        {error && <Alert color = 'failure' className='mt-5'>{error}</Alert>}
+        </form>
+      )}
+      
+    </div>
+  );
+}
