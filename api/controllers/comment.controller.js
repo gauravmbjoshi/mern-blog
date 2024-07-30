cf8e6b3bbd2156d46af0faa5093ac8233460aa02
Gaurav Joshi <79218309+gauravmbjoshi@users.noreply.github.com>
2024-07-30 15:20:11 +0530

    add like functionality to comment component

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -13,7 +13,7 @@ export const createComment = async (req, res, next) => {
 
     const newComment = new Comment({
       content,
-      postId,
+      postId, 
       userId,
     });
     await newComment.save();
@@ -33,3 +33,23 @@ export const getComment = async(req, res, next) =>{
     next(error);
   }
 }
+export const likeComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return next(errorHandler(404, "Comment not found"));
+    }
+    const userIndex = comment.likes.indexOf(req.user.id);
+    if (userIndex === -1) {
+      comment.numberOfLikes += 1;
+      comment.likes.push(req.user.id);
+    } else {
+      comment.numberOfLikes -= 1;
+      comment.likes.splice(userIndex, 1);
+    }
+    await comment.save();
+    res.status(200).json(comment);
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,14 +1,15 @@
 import { Button, Textarea, Alert } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 import Comment from './Comment';
 
 export default function CommentSection({postId}) {
 const {currentUser} = useSelector(state => state.user);
 const [comment, setComment] = useState('');
 const [error,setError]=useState(null);
-const [ comments,setComments] = useState([]);
+const [comments,setComments] = useState([]);
+const navigate = useNavigate();
 const handleAddComment = async(e)=>{
     e.preventDefault();
     if(comment.length>200){
@@ -49,6 +50,34 @@ useEffect(()=>{
   }
   getComments();
 },[postId])
+
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate("/sign-in");
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: "PUT",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className='max-w-3xl mx-auto w-full my-5 p-3 border rounded border-slate-300'>
       {currentUser ? (
@@ -121,6 +150,7 @@ useEffect(()=>{
             <Comment
               key={comment._id}
               comment={comment}
+              onLike={handleLike}
             />
           ))}
         </>
