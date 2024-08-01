0bfa5a05ef7f674cfc34ff895167af21f3957539
Gaurav Joshi <79218309+gauravmbjoshi@users.noreply.github.com>
2024-08-01 14:25:40 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,7 +6,9 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoute from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from 'path';
 dotenv.config();
+const __dirname = path.resolve(); 
 const app = express();
 
 mongoose
@@ -20,12 +22,18 @@ mongoose
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoute);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
